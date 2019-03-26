11b54ca5374d0fc4e78688dc1fa90d0e67bd1cef
Mario Daskalov <mariodaskalov@gmail.com>
2019-03-26 17:24:36 +0200

    Add new task card when clicking add new task button

--- client.js
+++ client.js
@@ -1,18 +1,69 @@
 var Promise = TrelloPowerUp.Promise;
 
+var CARD_TEMPLATE = `
+# What
+
+_Describe in detail what is the goal of the task and what we want to achieve._
+
+- Feature flags
+- Analytics
+- Tooltips
+
+# Why
+
+_Describe why are we doing this task. What problem are we solving to our customers or business._
+
+# Design
+
+_List all links to the finished design._
+
+# Engineering
+
+__PR__: _Link to the GitHub pull request after the task has been implemented_
+
+__Sprint estimate__: _Estimate how long the task will take to be completed_
+__Sprint spent__: _Total hours spent on the task_
+
+__QA__: _The person responsible for doing QA_
+__QA Estimate__: _Total hours estimated for QA_
+__QA Spent__: _Total hours spent in QA_
+
+__Total estimate__: _Total hours estimate_
+__Total Spent__: _Total hours spent_
+`;
+
+function addNewTask(listId) {
+  Trello.post('/cards/', {
+    name: 'New card',
+    desc: CARD_TEMPLATE,
+    idList: listId,
+    pos: 'top'
+  });
+}
+
 TrelloPowerUp.initialize({
   'board-buttons': function(t, options){
     return [
       {
         text: 'Add New Task',
         callback: function(t){
-          Trello.authorize({
-            type: 'popup',
-            name: 'Authorize Power Up to Add Cards',
-            scope: {read: true, write: true},
-            success: console.log,
-            error: console.log
-          });
+          t.lists('id', 'name').then(function(lists) {
+            var list = lists.find(list => {
+              return list.name === 'Ready For Development' 
+            });
+
+            if (Trello.authorized()) {
+              return addNewTask(list.id);
+            }
+
+            Trello.authorize({
+              type: 'popup',
+              name: 'Authorize Power Up to Add Cards',
+              scope: {read: true, write: true},
+              success: () => addNewTask(list.id),
+              error: console.log
+            });
+          })
         }
       },
       {
